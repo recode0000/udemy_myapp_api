dffb56e87dca0b533155508622c709da751f05a4
Reo Ito <leo.0210.ito@gmail.com>
2022-12-04 16:41:14 +0900

    fix

--- app/controllers/api/v1/hello_controller.rb
+++ app/controllers/api/v1/hello_controller.rb
@@ -1,7 +1,9 @@
 module Api
   module V1
     class HelloController < ApplicationController
-      render json: 'Hello by Leo'
+      def index
+        render json: 'Hello by Leo'
+      end
     end
   end
 end
