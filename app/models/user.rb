00543b152557cf1a6fb2ebd63651a873c2491312
Reo Ito <leo.0210.ito@gmail.com>
2022-12-08 00:28:47 +0900

    I18nの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
 
   validates :name, presence: true, length: { maximum: 30, allow_blank: true }
-  validates :password, presence: true, length: { minimum: 8 },
-    format: { with: VALID_PASSWORD_REGEX }, allow_nil: true
+  validates :password, presence: true, length: { minimum: 8 }, allow_nil: true,
+    format: {
+      with: VALID_PASSWORD_REGEX,
+      message: :invalid_password
+    }
   validates :is_active, inclusion: { in: [ true, false ] }
   validates :is_admin, inclusion: { in: [ true, false ] }
 end
